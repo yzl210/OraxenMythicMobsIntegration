be781e47794b162c7529c3362a1e744f4bdd9cf1
yzl210 <2846840578@qq.com>
2020-06-15 20:14:41 +0800

    Use a new way to drop items and equip items to prevent log error and more stable, update README.md and plugin.yml

--- README.md
+++ README.md
@@ -2,18 +2,31 @@
 
 It's a bukkit plugin that can support [MythicMobs](https://www.spigotmc.org/resources/5702) to drop [Oraxen](https://www.spigotmc.org/resources/72448) items.
 
-Current Verion: v2.0
+Current Verion: v3.0
 
-Designed for MythicMobs v4.9.1 and Oraxen v1.29.0. It maybe support lower or higher versions.
+Tested Version:  
+MythicMobs v4.9.1  
+Oraxen v1.29.0
 
-# Custom Drops
-`oraxen [oraxen item name] [amount to drop(number or region)] [chance to drop(0-100)]`  
+#  Drops
+`oraxen [oraxen item name] [amount to drop(number or region)] [chance to drop(0-1)]`  
 For Example:  
-`oraxen custom_material 3-4 70`  
-means **70** percent chance to drop **3 to 4 custom_material** item.
-# Custom Equipment
-`oraxen [oraxen item(equipment) name]:[slot(0,1,2,3,4,5)]`  
-**0** is **MainHand**, **1** is **OffHand**, **2** is **Helmet**, **3** is **ChestPlate**,  **4** is **Leggings**, **5** is **Boots**.  
+`oraxen custom_material 3-4 0.75`  
+means **75** percent chance to drop **3 to 4 custom_material** items.
+#  Equipment
+`oraxen [slot] [oraxen item(equipment) name]`  
+Slots:  
+* 0, mainhand, helmet
+* 1, boots, shoes
+* 2, leggings, pants
+* 3, chestpiece, chestplate, body
+* 4, helmet, helm   
+* 5, shield  
+
+_5 is OffHand, but you can't put **offhand**, because MythicMobs's Author didn't put the alias._
+
 For Example:  
-`oraxen custom_sword:0`  
-means put a **custom_sword** in **MainHand**.
+`oraxen mainhand custom_sword`  
+means put a **custom_sword** item in **MainHand**.  
+`oraxen 3 custom_chestplate`  
+means put a **custom_chestplate** item in **ChestPlate** Slot
--- src/cn/leomc/OraxenMythicMobsIntegration/OraxenMythicMobsIntegration.java
+++ src/cn/leomc/OraxenMythicMobsIntegration/OraxenMythicMobsIntegration.java
@@ -1,18 +1,14 @@
 package cn.leomc.OraxenMythicMobsIntegration;
 
-import io.lumine.xikage.mythicmobs.api.bukkit.events.MythicMobLootDropEvent;
-import io.lumine.xikage.mythicmobs.api.bukkit.events.MythicMobSpawnEvent;
+import io.lumine.xikage.mythicmobs.adapters.bukkit.BukkitItemStack;
+import io.lumine.xikage.mythicmobs.api.bukkit.events.MythicDropLoadEvent;
+import io.lumine.xikage.mythicmobs.drops.droppables.ItemDrop;
 import io.th0rgal.oraxen.items.OraxenItems;
 import org.bukkit.Bukkit;
-import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.inventory.EntityEquipment;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import java.util.List;
-import java.util.Random;
 
 public class OraxenMythicMobsIntegration extends JavaPlugin implements Listener {
 
@@ -23,85 +19,21 @@ public class OraxenMythicMobsIntegration extends JavaPlugin implements Listener 
 
     @Override
     public void onDisable() {
-
     }
 
     @EventHandler
-    public void onMythicMobLootDrop(MythicMobLootDropEvent mmlde) {
-
-        List<String> mmdrops = mmlde.getMobType().getConfig().getStringList("Drops");
-        for(String str : mmdrops) {
-            if(str.contains("oraxen") && str.split(" ").length == 4 && OraxenItems.isAnItem(str.split(" ")[1])) {
-                ItemStack odrops = OraxenItems.getItemById(str.split(" ")[1]).build();
-                String am = str.split(" ")[2];
-                int amount = 1;
-                if(am.split("-").length == 2) {
-                    amount = getRandomNumber(Integer.parseInt(am.split("-")[0]),Integer.parseInt(am.split("-")[1]));
-
-                }
-                if(getChance(Integer.parseInt(str.split(" ")[3])))
-                    for(int i = 0;i < amount;i++)
-                        mmlde.getEntity().getWorld().dropItem(mmlde.getEntity().getLocation(), odrops);
+    public void onMythicDropLoadEvent(MythicDropLoadEvent mdle) {
+        if (mdle.getDropName().equalsIgnoreCase("oraxen")) {
+            String line = mdle.getContainer().getLine();
+            if (line.split(" ").length == 4 && OraxenItems.isAnItem(line.split(" ")[1])) {
+                ItemStack od = OraxenItems.getItemById(line.split(" ")[1]).build();
+                ItemDrop itemDrop = new ItemDrop(line, mdle.getConfig(), new BukkitItemStack(od));
+                mdle.register(itemDrop);
+            } else if (line.split(" ").length == 3 && OraxenItems.isAnItem(line.split(" ")[2])) {
+                ItemStack od = OraxenItems.getItemById(line.split(" ")[2]).build();
+                ItemDrop itemDrop = new ItemDrop(line, mdle.getConfig(), new BukkitItemStack(od));
+                mdle.register(itemDrop);
             }
-
         }
     }
-
-    @EventHandler
-    public void onMythicMobSpawn(MythicMobSpawnEvent mmse) {
-
-        List<String> mmequipment = mmse.getMobType().getConfig().getStringList("Equipment");
-        for(String str : mmequipment) {
-            if(str.contains("oraxen") && str.split(" ").length == 2 && OraxenItems.isAnItem(str.split(" ")[1].split(":")[0])) {
-                ItemStack oequip = OraxenItems.getItemById(str.split(" ")[1].split(":")[0]).build();
-                LivingEntity mob = (LivingEntity) mmse.getEntity();
-                EntityEquipment mmequip = mob.getEquipment();
-                switch(str.split(" ")[1].split(":")[1]) {
-                    case "0":
-                        mmequip.setItemInMainHand(oequip);
-                        mmequip.setItemInMainHandDropChance(0);
-                        break;
-                    case "1":
-                        mmequip.setItemInOffHand(oequip);
-                        mmequip.setItemInOffHandDropChance(0);
-                        break;
-                    case "2":
-                        mmequip.setHelmet(oequip);
-                        mmequip.setBootsDropChance(0);
-                        break;
-                    case "3":
-                        mmequip.setChestplate(oequip);
-                        mmequip.setLeggingsDropChance(0);
-                        break;
-                    case "4":
-                        mmequip.setLeggings(oequip);
-                        mmequip.setChestplateDropChance(0);
-                        break;
-                    case "5":
-                        mmequip.setBoots(oequip);
-                        mmequip.setHelmetDropChance(0);
-                        break;
-                }
-
-            }
-
-
-        }
-
-    }
-
-
-    public boolean getChance(int percentage){
-        int r = new Random().nextInt(99);
-        if(r >=0 && r < percentage)
-            return true;
-        else
-            return false;
-    }
-
-
-    public static int getRandomNumber(int min, int max) {
-        return new Random().nextInt(max) % (max - min + 1) + min;
-    }
-
 }
